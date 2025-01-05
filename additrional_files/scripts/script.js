428399847c3e9ae4c5e82b541070e7640cd269bb
ZoltiiSens <zoltiisen@gmail.com>
2025-01-05 21:35:39 +0200

    Initial commit

---
+++ additrional_files/scripts/script.js
@@ -0,0 +1,14 @@
+const videoButtons = document.querySelectorAll('.video-button');
+const videoPlayer = document.getElementById('video-player');
+
+// videoPlayer.play();
+
+videoButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		const newSource = button.getAttribute('video-source');
+		const sourceElement = videoPlayer.querySelector('source');
+		sourceElement.src = newSource;
+		videoPlayer.load();
+		videoPlayer.play();
+	});
+});
